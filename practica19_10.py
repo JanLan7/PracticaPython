604fe43cc0490663e2c45f35d23e127f04ca91af
JanLan <idiomasjanlan@gmail.com>
2024-10-19 23:25:19 -0300

    agregando cambios

--- practica19_10.py
+++ practica19_10.py
@@ -1,5 +1,5 @@
-miembros_iniciales = int(input())
-dias_observacion = int(input())
+miembros_iniciales = int(input("Introduce el número inicial de miembros del grupo: "))
+dias_observacion = int(input("Introduce el número de días de observación: "))
 
 for dia in range(1, dias_observacion + 1):
     miembros_iniciales *= 2
